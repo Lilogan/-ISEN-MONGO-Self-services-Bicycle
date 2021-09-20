244ae7201493f059aa1f5e739a36f8c920647853
Lilogan <lilianhouplin@hotmail.fr>
2021-09-20 14:18:30 +0200

    Adding database connection with .env file

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def connectDB():
+    url = os.environ.get('databaseUrl')
+    user = os.environ.get('databaseUser')
+    password = os.environ.get('databaseKey')
+
+    return MongoClient("mongodb+srv://" + user + ":" + password + "@" + url + "/?retryWrites=true&w=majority")
+
 def main():
-    print("Hello World!")
+    db = connectDB()
 
 if __name__ == "__main__":
     main()
